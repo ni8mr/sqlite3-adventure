b6e7c2f13727fb3132f5735fe5077f6265d6223d
ni8mr <MY_NAME@example.com>
2015-05-03 17:34:36 +0600

    Selecting and printing Data

---
+++ sqlc.py
@@ -0,0 +1,22 @@
+# SELECT statement  with and without unicode characters
+
+import sqlite3
+
+with sqlite3.connect("new.db") as conn:
+	c = conn.cursor()
+
+	# using a for loop to iterate through the database, printing the
+	# results line by line
+	for row in c.execute("SELECT * FROM population"):
+		print row
+
+	c.execute("SELECT * FROM population")
+
+	# fetchall() retrieves all records from the query
+	rows = c.fetchall()
+
+	# output the rows to the screen, row by row
+
+	for r in rows:
+		print r[0], r[1]
+
